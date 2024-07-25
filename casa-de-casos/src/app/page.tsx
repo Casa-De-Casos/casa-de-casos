38dd186f667463b2558507c8c66df1eaccec6d76
M10mo <mohdkanoongo@gmail.com>
2024-07-25 13:04:56 -0400

    more landing page details added

--- casa-de-casos/src/app/page.tsx
+++ casa-de-casos/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check } from "lucide-react";
+
 export default function Home() {
   return (
     <div className="bg-slate-50 ">
@@ -8,8 +10,35 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/" />
+                <img src="/phone-logo.png" className="w-full" />
               </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                Phone Case!
+              </h1>
+              <p className="mt-8 tex-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                {" "}
+                Capture your favorite memories with your own phone case! Casa de
+                Casos allows you to protect more than just your cellular device.
+              </p>
+
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    High-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Long lasting prints
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Latest phone models supported
+                  </li>
+                </div>
+              </ul>
             </div>
           </div>
         </MaxWidthWrapper>
