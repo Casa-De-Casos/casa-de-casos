9b8d816bcd6015746f1ba5ea1f50626d4c46644b
M10mo <mohdkanoongo@gmail.com>
2024-07-30 14:54:14 -0400

    animation landing page

--- casa-de-casos/src/app/page.tsx
+++ casa-de-casos/src/app/page.tsx
@@ -116,6 +116,43 @@ export default function Home() {
             </h2>
             <img src="/logo.jpg" className="w-24 order-0 lg:order-2" />
           </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600 " />
+                <Star className="h-5 w-5 text-green-600 fill-green-600 " />
+                <Star className="h-5 w-5 text-green-600 fill-green-600 " />
+                <Star className="h-5 w-5 text-green-600 fill-green-600 " />
+                <Star className="h-5 w-5 text-green-600 fill-green-600 " />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case is very durable. I have had it for six months now,
+                  and the image is still{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    super clear
+                  </span>
+                  . On the case I had before, the image began to fade after a
+                  couple of weeks.
+                </p>
+              </div>
+              <div className="flex gap-4 mat-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover "
+                  src="/users/user-1.png"
+                  alt="user1"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Mark</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </div>
